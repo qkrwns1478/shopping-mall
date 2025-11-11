d5dcc7d02065052ee1cd8fc0e4e844a761869c0b
qkrwns1478 <qkrwns1478@gmail.com>
2025-11-11 19:57:13 +0900

    feat: add bootstrap template

--- src/main/java/com/example/shoppingmall/web/controller/HomeController.java
+++ src/main/java/com/example/shoppingmall/web/controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.example.shoppingmall.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
-        return "Welcome!";
-    }
-    @GetMapping("/hello")
-    public String hello() {
-        return "HelloWorld";
+    public String home(Model model) {
+        return "index";
     }
 }
